4982e3e86ffe1c577015687fbca0c8f20d470fec
Pablo Solano <pablo.a.solano@gmail.com>
2021-06-01 22:40:47 -0400

    fixed interactions (detection and pausing during conversation)

--- scripts/town/Bureaucrat1.gd
+++ scripts/town/Bureaucrat1.gd
@@ -1,19 +1,25 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 export (int) var speed = 500
 export (float) var time_inside = 3
 
+# pathfinding variables
 var velocity = Vector2()
 var going_home = true
 var inside_timer = 0
 
+# references
+var dialoguePopup
+var player
+
+var dialogue_state = 0
+
+func _ready():
+	dialoguePopup = get_tree().root.get_node("Root/CanvasLayer/DialoguePopup")
+	player = get_tree().root.get_node("Root/Player")
+
 # state machine
 # check position, then update seen/velocity
-
 func update_velocity(delta):
 	velocity = Vector2()
 	# check if NPC is inside
@@ -66,3 +72,45 @@ func update_velocity(delta):
 func _physics_process(delta):
 	update_velocity(delta)
 	velocity = move_and_slide(velocity)
+	
+func talk(answer = ""):
+	# Set dialoguePopup npc to Bob
+	dialoguePopup.npc = self
+	dialoguePopup.npc_name = "Bob"
+	
+	# Show the current dialogue
+
+	match dialogue_state:
+		0:
+			# Update dialogue tree state
+			dialogue_state = 1
+			# Show dialogue popup
+			dialoguePopup.dialogue = "Hello adventurer! I lost my necklace, can you find it for me?"
+			dialoguePopup.answers = "[A] Yes  [B] No"
+			dialoguePopup.open()
+		1:
+			match answer:
+				"A":
+					# Update dialogue tree state
+					dialogue_state = 2
+					# Show dialogue popup
+					dialoguePopup.dialogue = "Thank you!"
+					dialoguePopup.answers = "[A] Bye"
+					dialoguePopup.open()
+				"B":
+					# Update dialogue tree state
+					dialogue_state = 3
+					# Show dialogue popup
+					dialoguePopup.dialogue = "If you change your mind, you'll find me here."
+					dialoguePopup.answers = "[A] Bye"
+					dialoguePopup.open()
+		2:
+			# Update dialogue tree state
+			dialogue_state = 0
+			# Close dialogue popup
+			dialoguePopup.close()
+		3:
+			# Update dialogue tree state
+			dialogue_state = 0
+			# Close dialogue popup
+			dialoguePopup.close()
--- scripts/town/Player.gd
+++ scripts/town/Player.gd
@@ -5,17 +5,31 @@ export (int) var speed = 500
 var velocity = Vector2()
 
 func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	velocity = velocity.normalized() * speed
+	# NPC Interaction
+	if Input.is_action_pressed("ui_select"):
+		var target = $RayCast2D.get_collider()
+		if target != null:
+			if target.is_in_group("NPCs"):
+				# Talk to NPC
+				target.talk()
+				return
 
 func _physics_process(delta):
+	# Get player input
+	var direction: Vector2
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	# If input is digital, normalize it for diagonal movement
+	if abs(direction.x) == 1 and abs(direction.y) == 1:
+		direction = direction.normalized()
+	
+	# Apply movement
+	var movement = speed * direction * delta
+	move_and_collide(movement)
+	
+	# Turn RayCast2D toward movement direction
+	if direction != Vector2.ZERO:
+		$RayCast2D.cast_to = direction.normalized() * 52
+	
 	get_input()
-	velocity = move_and_slide(velocity)
