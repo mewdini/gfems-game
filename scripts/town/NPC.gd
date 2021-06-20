eedadd32fadde87df481f16d29d268eab46cc1d7
Paul Vierthaler <pavierthaler@wm.edu>
2021-06-20 17:58:56 -0400

    fix animations

--- scripts/town/NPC.gd
+++ scripts/town/NPC.gd
@@ -138,10 +138,10 @@ func talk(answer = ""):
 	var player_to_npc_y_delta = player.position.y - self.position.y
 	
 	# if player is further on the x axis, then the rotation should be on x axis
-	if abs(player_to_npc_x_delta) > abs(player_to_npc_y_delta):
-		if player_to_npc_x_delta > 0:
+	if abs(player_to_npc_x_delta) >= abs(player_to_npc_y_delta):
+		if player_to_npc_x_delta < 0:
 			$AnimatedSprite.play("idle_left")
-		elif player_to_npc_x_delta < 0:
+		elif player_to_npc_x_delta > 0:
 			$AnimatedSprite.play("idle_right")
 	else:
 		if player_to_npc_y_delta < 0:
@@ -166,19 +166,19 @@ func talk(answer = ""):
 	if dialogue_state == "-1":
 		dialogue_state = "0"
 		dialogue_popup.close()
-		# Keep track of the number of interactions between player and NPCs
+		# Keep track of the number of interactions between player and NPCs. Set
+		# to add when the conversation ends.
 		if npc_name in PlayerData.conversations_held:
 			PlayerData.conversations_held[npc_name] += 1
 		else:
 			PlayerData.conversations_held[npc_name] = 1
-		print(PlayerData.conversations_held)
 		return
 	
 	
 	
 	# Set possible buttons
 	# TODO don't hardcode this here
-	var buttons = ['Z', 'X', 'C', 'V']
+	var buttons = ['Z', 'X', 'C', 'V', 'B']
 	
 	var ans
 	
