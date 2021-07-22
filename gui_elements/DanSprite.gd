24ea8411855a1acdb8e21ecae18a47d8aa3bf58f
Pablo Solano <pablo.a.solano@gmail.com>
2021-07-22 01:20:00 -0700

    updated shaders and title

--- gui_elements/DanSprite.gd
+++ gui_elements/DanSprite.gd
@@ -5,6 +5,9 @@ onready var shader = preload("res://shaders/aura.shader")
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		if get_rect().has_point(to_local(event.position)):
+			for i in self.get_parent().get_children():
+				if i != self:
+					i.material = null
 			self.material = ShaderMaterial.new()
 			self.material.shader = shader
 			PlayerData.set_sprite(PlayerData.Sprite_Name.DAN)
