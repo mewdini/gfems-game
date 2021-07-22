c868a57a19039d56cfda2b95c27ae9a403e00b5f
Pablo Solano <pablo.a.solano@gmail.com>
2021-07-22 00:57:21 -0700

    added outlines to character selection

--- gui_elements/AmeliaSprite.gd
+++ gui_elements/AmeliaSprite.gd
@@ -1,6 +1,10 @@
 extends Sprite
 
+onready var shader = preload("res://shaders/aura.shader")
+
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		if get_rect().has_point(to_local(event.position)):
+			self.material = ShaderMaterial.new()
+			self.material.shader = shader
 			PlayerData.set_sprite(PlayerData.Sprite_Name.AMELIA)
--- gui_elements/PlayButton.gd
+++ gui_elements/PlayButton.gd
@@ -4,8 +4,13 @@ extends Button
 export(String, FILE) var next_scene_path: = ""
 
 func _on_button_up():
+	wipe_save_data()
 	get_tree().set_pause(false)
 	get_tree().change_scene(next_scene_path)
 
 func _get_configuration_warning():
 	return "next_scene_path must be set" if next_scene_path == "" else ""
+	
+func wipe_save_data():
+	var dir = Directory.new()
+	dir.remove("user://state.json")
