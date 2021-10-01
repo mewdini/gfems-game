2075ffc7066311e7d030f8872ce2710965e60ece
Pablo Solano <pablo.a.solano@gmail.com>
2021-10-01 19:40:57 -0400

    more progress to minimap

--- gui_elements/MiniMap.gd
+++ gui_elements/MiniMap.gd
@@ -1,16 +1,19 @@
 extends TextureRect
 
+export var zoom_out: float  
 onready var player: KinematicBody2D = find_node("Player")
+# TODO make texture from full image
 func _update():
 	# get player coordinates
 	var player_pos: Vector2 = player.global_position
 	# find relative minimap coordinates
 	var mini_pos: Vector2 = player_pos / 5
 	# make rectangle based on player (center) coordinates
-	var width = 5 # TODO decide zoom level
-	var height = 5 # TODO decide zoom level
+	var width = zoom_out
+	var height = zoom_out
 	var view_rect: Rect2 = Rect2(mini_pos.x, mini_pos.y, width, height)
 	# TODO offset so coordinates are not outside of minimap bounds
-	# make texture from rectangle and image
+	# make atlastexture from rectangle and texture
+	#
 	# draw texture on this node
-	pass
+	# self.set_value(texture)
