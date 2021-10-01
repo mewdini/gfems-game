60c56b1fccb797f1e82802387993fe4c5990490f
Pablo Solano <pasolano@email.wm.edu>
2021-10-01 16:10:20 -0400

    progress on minimap

--- gui_elements/MiniMap.gd
+++ gui_elements/MiniMap.gd
@@ -5,8 +5,12 @@ func _update():
 	# get player coordinates
 	var player_pos: Vector2 = player.global_position
 	# find relative minimap coordinates
+	var mini_pos: Vector2 = player_pos / 5
 	# make rectangle based on player (center) coordinates
-	# offset so coordinates are not outside of minimap bounds
+	var width = 5 # TODO decide zoom level
+	var height = 5 # TODO decide zoom level
+	var view_rect: Rect2 = Rect2(mini_pos.x, mini_pos.y, width, height)
+	# TODO offset so coordinates are not outside of minimap bounds
 	# make texture from rectangle and image
-	# draw texture on this
+	# draw texture on this node
 	pass
